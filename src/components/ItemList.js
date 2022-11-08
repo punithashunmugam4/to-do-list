40b53db7b9c359c33b0d311827a69644b1bb2549
PUNITHA SHUNMUGAM S <110711185+punithashunmugam4@users.noreply.github.com>
2022-11-08 11:19:48 +0530

    Update ItemList.js

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,10 +1,10 @@
 import Task from "./Task"
 
-const ItemList=({tasks,handleDoubleClick,handleDelete})=>{
+const ItemList=({tasks,handleDoubleClick,setEditableId,editableId,handleEditing,handleDelete})=>{
     return (
         <div className='items-list'>
-        {tasks.map((task)=><Task  key={task.id} task={task} 
-        handleDoubleClick={handleDoubleClick} handleDelete={handleDelete}/>)
+        {tasks.map((task)=><Task key={task.id} task={task}
+        handleDoubleClick={handleDoubleClick} handleEditing={handleEditing} handleDelete={handleDelete} setEditableId={setEditableId} editableId={editableId}/>)
          }
         </div>
     );
