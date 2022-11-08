522beb58b1feea08380c32b83a4f45921b3d6257
PUNITHA SHUNMUGAM S <110711185+punithashunmugam4@users.noreply.github.com>
2022-11-08 11:20:26 +0530

    Update Task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,10 +1,16 @@
 
-const Task=({task,handleDoubleClick,handleDelete})=>{
+const Task=({task,handleDoubleClick,setEditableId,editableId,handleEditing,handleDelete})=>{
+  const handleSubmission=(e)=>{
+    e.target.style.display="none"
+    setEditableId("");
+  }
     return(
         <div className='to-do-item'>
-          <span className='name' style={task.complete ? {textDecoration:"line-through"}:null} 
-          onDoubleClick={()=>handleDoubleClick(task.id)}>{task.taskName}</span>
+          <span className='name' key={task.id} id={task.id} style={task.complete ? {textDecoration:"line-through"}:null} 
+          onDoubleClick={()=>handleDoubleClick(task.id)} suppressContentEditableWarning={true} contentEditable={editableId===task.id ? true:false} >{task.taskName}</span>
           <div className='action'>
+          <button id={'edit'+task.id} style={{display: editableId===task.id?"inline":"none"}} onClick={(e)=>handleSubmission(e)}>Enter</button>
+            <button onClick={()=>handleEditing(task.id)}>Edit</button>
             <span><button onClick={()=>handleDelete(task.id)}>Delete</button></span>
           </div>
         </div>)
